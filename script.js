3ba7cfc4c719585e15f5f9cc295a8021ce77e2cc
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-05-07 11:12:02 -0400

    in script.js scroll event listener, add line to sections.forEach to remove change class styling on other nav links so only appropriate section has navbar link styling

--- script.js
+++ script.js
@@ -15,7 +15,10 @@ window.addEventListener('scroll', () => {
 
   sections.forEach((section, i) => {
     if(window.pageYOffset >= section.offsetTop - 10) {
+      navbarLinks.forEach(navbarLink => {
+        navbarLink.classList.remove('change');
+      });
       navbarLinks[i].classList.add('change');
     }
-  })
+  });
 });
