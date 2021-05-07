5940b2a7202b049fc0734b24f32127e8b5c7c960
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-05-07 11:04:24 -0400

    in script.js create variables sections and navbarLinks, in style.css add styling classes change.navbar-link and chance.navbar-link::before

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const navbar = document.querySelector('.navbar');
 
-const navbarOffsetTop = navbar.offsetTop
+const navbarOffsetTop = navbar.offsetTop;
+
+const sections = document.querySelectorAll('section');
+
+const navbarLinks = document.querySelectorAll('.navbar-link')
 
 window.addEventListener('scroll', () => {
   if(window.pageYOffset >= navbarOffsetTop) {
