7093da0afb54889c7565bc5e91d992db8f96d349
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-06-09 10:24:25 -0400

    in themeButton event listener, add setItem on localStorage for getCurrentTheme and getCurrentIcon

--- script.js
+++ script.js
@@ -42,4 +42,6 @@ if(selectedTheme){
 themeButton.addEventListener('click', () => {
   document.body.classList.toggle(darkTheme)
   themeButton.classList.toggle(iconTheme)
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
 })
