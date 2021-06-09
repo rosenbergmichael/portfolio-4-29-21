51f0711411c899632c6a3b692033492b02196a6a
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-06-09 10:17:04 -0400

    in script.js create variables getCurrentTheme and getCurrentIcon

--- script.js
+++ script.js
@@ -31,6 +31,9 @@ const iconTheme = 'bx-sun'
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark':'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon':'bx-sun'
+
 themeButton.addEventListener('click', () => {
   document.body.classList.toggle(darkTheme)
   themeButton.classList.toggle(iconTheme)
